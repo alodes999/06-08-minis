8906df118b41fefcb9aabbda2fe2fc6543f07029
Andrew Lodes <alodes999@gmail.com>
2015-06-08 16:05:58 -0500

    Add initialize/documentation for phone_number_format
    initial documentation and initialize method for the phone_number_format
    class.

--- phone_number_format.rb
+++ phone_number_format.rb
@@ -1,2 +1,14 @@
 # PhoneNumberFormatter
 # Takes "1234567890"; returns "123-456-7890" or "(123) 456-7890".
+
+class PhoneNumberFormatter
+  # Our initialize method for PhoneNumberFormatter.  We pass a phone number as a string into the class. This string
+  # is our 1 argument for this class
+  # 
+  # We have 1 attribute
+  # => @phone_number_to_format = a string of the phone number we want to format
+  def initialize(phone_number_to_format)
+    @phone_number_to_format = phone_number_to_format
+  end
+  
+end
