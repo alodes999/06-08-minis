355e58e42839a30a14cc832b2d13c01b01007185
Andrew Lodes <alodes999@gmail.com>
2015-06-08 13:00:09 -0500

    Initial commit to git repository for mini-projects

---
+++ paragraph_truncator.rb
@@ -0,0 +1,3 @@
+# ParagraphTruncator
+# Takes a long string (i.e. a paragraph) and truncates it to X characters and sticks a ... at the end.
+# Should be able to take an argument to define X and also define an alternative set of characters for the ... (e.g. "Read More")
---
+++ word_connector.rb
@@ -0,0 +1,3 @@
+# WordConnector
+# Takes ["Blue", "Red", "Green"]; returns "Blue, Red, and Green".
+# Takes ["Blue", "Red"]; returns "Blue and Red".
