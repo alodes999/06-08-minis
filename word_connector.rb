96dcb76d75dee77abbf9f3c6573aa1ed5832d455
Andrew Lodes <alodes999@gmail.com>
2015-06-08 13:30:27 -0500

    Added initialize and last_word method
    Last word method pulls out the last word and appends "and"

--- word_connector.rb
+++ word_connector.rb
@@ -1,3 +1,16 @@
 # WordConnector
 # Takes ["Blue", "Red", "Green"]; returns "Blue, Red, and Green".
 # Takes ["Blue", "Red"]; returns "Blue and Red".
+
+class WordConnector
+  
+  def initialize(array_to_split)
+    @array_to_split = array_to_split
+  end
+  
+  def last_word
+    @lastword = @array_to_split.pop
+  end
+  
+  
+end
